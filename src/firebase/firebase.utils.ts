b9d3f272d8ff62efc90e75f57b409c343d8daa9f
d1eshi <silva.diegoF03@gmail.com>
2021-09-17 20:33:46 -0300

    config...

--- src/firebase/firebase.utils.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCLBa0phjdk_FD3nqMdzWomRH6wNkfXr_M',
-  authDomain: 'clone-37bfb.firebaseapp.com',
-  projectId: 'clone-37bfb',
-  storageBucket: 'clone-37bfb.appspot.com',
-  messagingSenderId: '140401943815',
-  appId: '1:140401943815:web:df8e8dbc6549f793b951a2',
-}
