15ece2536d761902a7b16c810c3cfc6711154759
d1eshi <silva.diegoF03@gmail.com>
2021-09-17 20:29:27 -0300

    config...

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,20 +1,11 @@
 /* eslint-disable no-unused-vars */
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCLBa0phjdk_FD3nqMdzWomRH6wNkfXr_M',
-  authDomain: 'clone-37bfb.firebaseapp.com',
-  projectId: 'clone-37bfb',
-  storageBucket: 'clone-37bfb.appspot.com',
-  messagingSenderId: '140401943815',
-  appId: '1:140401943815:web:df8e8dbc6549f793b951a2',
-}
+import { firebaseConfig } from './firebase.utils'
 
 const app = initializeApp(firebaseConfig)
 
-// const db = getFirestore(app)
 const auth = getAuth()
 
 export { auth }
---
+++ src/firebase/firebase.utils.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCLBa0phjdk_FD3nqMdzWomRH6wNkfXr_M',
+  authDomain: 'clone-37bfb.firebaseapp.com',
+  projectId: 'clone-37bfb',
+  storageBucket: 'clone-37bfb.appspot.com',
+  messagingSenderId: '140401943815',
+  appId: '1:140401943815:web:df8e8dbc6549f793b951a2',
+}
